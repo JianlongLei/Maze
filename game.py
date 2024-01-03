1c9ec468557858d793bb045d9dd5e06e7e5412a5
ifon <1565975426@qq.com>
2024-01-03 14:20:04 +0100

    use example 8

--- game.py
+++ game.py
@@ -7,16 +7,16 @@ class Game:
     def __init__(self, maze_map, start_position, end_position):
         self.maze_map = maze_map
         self.env = Environment(maze_map, start_position, end_position)
-        self.agent = GreedyQlearning(self.env)
+        # self.agent = GreedyQlearning(self.env)
         self.agent2 = DPQlearning(self.env)
         self.start_position = start_position
         self.width = self.env.width
         self.height = self.env.height
 
     def solve(self):
-        self.agent.train()
+        # self.agent.train()
         self.agent2.train()
-        result = self.agent.get_result(self.env.axisToState(self.start_position))
+        # result = self.agent.get_result(self.env.axisToState(self.start_position))
         res = self.agent2.get_result(self.env.axisToState(self.start_position))
 
-        return result
+        return res
