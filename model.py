1d1eabf897f6a289221f9957dbfd7f9f86916970
ifon <1565975426@qq.com>
2024-01-06 00:10:10 +0100

    add DQN model

---
+++ model.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+
+class DQNModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(DQNModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return
